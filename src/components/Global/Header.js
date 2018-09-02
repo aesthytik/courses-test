703195f63818f8b04bb304d0e77e7174e3d7bc46
aesthytik <rawat.vipin4818@gmail.com>
2018-09-03 01:16:10 +0530

    minor path fix

--- src/components/Global/Header.js
+++ src/components/Global/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import {Link} from 'gatsby';
 import styled from 'styled-components';
-import { darken } from 'polished';
+import {darken} from 'polished';
 import config from '../../utils/config';
 
 const Container = styled.section`
@@ -40,12 +40,12 @@ const SignupBtn = styled.div`
   transition: 0.3s;
   padding: 2.5rem 3.5rem !important;
   :hover {
-    border-color: ${darken(0.2, '#1dbe71')} !important;
-    color: ${darken(0.2, '#1dbe71')} !important;
+    border-color: ${darken (0.2, '#1dbe71')} !important;
+    color: ${darken (0.2, '#1dbe71')} !important;
   }
 `;
 
-const LoginBtn = styled(Link)`
+const LoginBtn = styled (Link)`
   border-left: 1px solid #5a6175;
   padding-left: 7%;
 `;
@@ -92,28 +92,28 @@ const NavbarBurger = styled.a`
 `;
 
 export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super (props);
 
-    this.toggleMobileMenu = this.toggleMobileMenu.bind(this);
+    this.toggleMobileMenu = this.toggleMobileMenu.bind (this);
     this.state = {
       isActive: false,
     };
   }
 
-  toggleMobileMenu() {
-    const { isActive } = this.state;
-    this.setState({
+  toggleMobileMenu () {
+    const {isActive} = this.state;
+    this.setState ({
       isActive: !isActive,
     });
   }
 
-  render() {
-    const { path } = this.props;
-    const { isActive } = this.state;
+  render () {
+    const {path} = this.props;
+    const {isActive} = this.state;
 
     let hasMemberIcon = false;
-    if (path === '/teacher-dashboard' || path === '/individual-pack') {
+    if (path === '/teacher-dashboard/' || path === '/individual-pack/') {
       hasMemberIcon = true;
     }
 
@@ -149,66 +149,78 @@ export default class Header extends React.Component {
                   <span aria-hidden="true" />
                 </NavbarBurger>
               </div>
-              {isActive ? (
-                <MobileMenu className="is-hidden-desktop has-text-centered">
-                  <aside className="menu">
-                    <ul className="menu-list is-size-6">
-                      <Link to="/sign-up/">
-                        <li>Sign up for free</li>
-                      </Link>
-                      <Link to="/">
-                        <li>Subscribe</li>
-                      </Link>
-                      <Link to="/login/">
-                        {' '}
-                        <li>Login</li>
-                      </Link>
-                      <Link to="/">
-                        {' '}
-                        <li>Help</li>
-                      </Link>
-                    </ul>
-                  </aside>
-                </MobileMenu>
-              ) : null}
+              {isActive
+                ? (
+<MobileMenu className="is-hidden-desktop has-text-centered">
+                    <aside className="menu">
+                      <ul className="menu-list is-size-6">
+                        <Link to="/sign-up/">
+                          <li>Sign up for free</li>
+                        </Link>
+                        <Link to="/">
+                          <li>Subscribe</li>
+                        </Link>
+                        <Link to="/login/">
+                          {' '}
+                          <li>Login</li>
+                        </Link>
+                        <Link to="/">
+                          {' '}
+                          <li>Help</li>
+                        </Link>
+                      </ul>
+                    </aside>
+                  </MobileMenu>
+)
+                : null}
 
               <LinkStyle className="navbar-menu">
                 <div className="navbar-end">
-                  {!hasMemberIcon ? (
-                    <React.Fragment>
-                      <a className="navbar-item primary-font-color">Foreword</a>
-                      <a className="navbar-item primary-font-color">Blog</a>
-                      <a className="navbar-item primary-font-color">Teachers</a>
-                      <a className="navbar-item primary-font-color">Freebies</a>
-                      <a className="navbar-item primary-font-color">
-                        Member's club
-                      </a>
-                      <LoginBtn
-                        to="/login/"
-                        className="navbar-item is-uppercase has-text-weight-bold"
-                      >
-                        Log in
-                      </LoginBtn>
-                      <Link to="/sign-up/" className="navbar-item">
-                        <SignupBtn className="button is-rounded is-outlined is-large is-pulled-right is-hover">
-                          <span className="is-size-6 has-text-weight-bold">
-                            SIGN UP FOR FREE
-                          </span>
-                        </SignupBtn>
-                      </Link>
-                    </React.Fragment>
-                  ) : (
-                    <React.Fragment>
-                      <a className="navbar-item primary-font-color">Home</a>
-                      <a className="navbar-item primary-font-color">Discover</a>
-                      <Name className="navbar-item is-uppercase has-text-weight-bold">
-                        #FNAME
-                      </Name>
-                      <Link to="/sign-up/" className="navbar-item">
-                        <img src="/images/admin-icon.svg" alt="admin" />
-                      </Link>
-                    </React.Fragment>
-                  )}
+                  {!hasMemberIcon
+                    ? (
+<React.Fragment>
+                        <a className="navbar-item primary-font-color">
+                          Foreword
+                        </a>
+                        <a className="navbar-item primary-font-color">Blog</a>
+                        <a className="navbar-item primary-font-color">
+                          Teachers
+                        </a>
+                        <a className="navbar-item primary-font-color">
+                          Freebies
+                        </a>
+                        <a className="navbar-item primary-font-color">
+                          Member's club
+                        </a>
+                        <LoginBtn
+                          to="/login/"
+                          className="navbar-item is-uppercase has-text-weight-bold"
+                        >
+                          Log in
+                        </LoginBtn>
+                        <Link to="/sign-up/" className="navbar-item">
+                          <SignupBtn className="button is-rounded is-outlined is-large is-pulled-right is-hover">
+                            <span className="is-size-6 has-text-weight-bold">
+                              SIGN UP FOR FREE
+                            </span>
+                          </SignupBtn>
+                        </Link>
+                      </React.Fragment>
+)
+                    : (
+<React.Fragment>
+                        <a className="navbar-item primary-font-color">Home</a>
+                        <a className="navbar-item primary-font-color">
+                          Discover
+                        </a>
+                        <Name className="navbar-item is-uppercase has-text-weight-bold">
+                          #FNAME
+                        </Name>
+                        <Link to="/sign-up/" className="navbar-item">
+                          <img src="/images/admin-icon.svg" alt="admin" />
+                        </Link>
+                      </React.Fragment>
+)}
                 </div>
               </LinkStyle>
             </nav>
