a6a2c605bfb7f69b28758eba2c253a67d30b10e1
Parminder Klair <perminder.klair@gmail.com>
2018-10-12 11:04:48 +0530

    update uri

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,14 +3,14 @@ const debug = process.env.NODE_ENV === 'development';
 module.exports = {
   debug,
 
-  siteName: 'Learn-Realm',
+  siteName: 'Learn Realm',
   author: 'Parminder Klair',
-  description: 'A Learning platform, bundled with awesome GatsbyJs.',
-  siteUrl: 'http://learn-realm.netlify.com',
+  description: 'A Learning platform.',
+  siteUrl: 'https://www.learnrealm.com',
 
   apiUrl: debug
     ? 'http://localhost:4000/graphql'
-    : 'https://api.learnrealm.com/graphql',
+    : 'https://www.learnrealm.com/graphql',
 
   twitter: '@NAME-HERE',
   fbUserId: '@NAME-HERE',
