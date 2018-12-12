e3eccfecebeec5122c7b617ffc6e783b1cd709db
aesthytik <rawat.vipin4818@gmail.com>
2018-12-12 17:22:35 +0530

    minor seo fixes

--- src/components/Global/Seo.js
+++ src/components/Global/Seo.js
@@ -27,28 +27,28 @@ const Seo = ({ title, description, url, image }) => {
   return (
     <Helmet>
       {/* General tags */}
-      <title>{pageTitle}</title>
+      {/* <title>{pageTitle}</title>
       <meta name="description" content={description} />
       <meta name="image" content={image} />
 
       {/* Schema.org tags */}
-      <script type="application/ld+json">
+      {/* <script type="application/ld+json">
         {JSON.stringify(schemaOrgJSONLD)}
-      </script>
+      </script> */} 
 
       {/* OpenGraph tags */}
-      <meta property="og:url" content={url} />
+      {/* <meta property="og:url" content={url} />
       <meta property="og:title" content={pageTitle} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
-      <meta property="fb:app_id" content={config.fbAppID} />
+      <meta property="fb:app_id" content={config.fbAppID} /> */}
 
       {/* Twitter Card tags */}
-      <meta name="twitter:card" content="summary_large_image" />
+      {/* <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={config.twitter} />
       <meta name="twitter:title" content={pageTitle} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
+      <meta name="twitter:image" content={image} /> */}
     </Helmet>
   );
 };
